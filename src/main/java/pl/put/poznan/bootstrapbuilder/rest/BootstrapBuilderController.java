b3638ae0d1ada5c763f64ccc3303094495eb6d85
virdiignis <gbryk1@gmail.com>
2019-11-17 20:25:35 +0100

    dummy endpoint

--- src/main/java/pl/put/poznan/bootstrapbuilder/rest/BootstrapBuilderController.java
+++ src/main/java/pl/put/poznan/bootstrapbuilder/rest/BootstrapBuilderController.java
@@ -1,10 +1,7 @@
-package pl.put.poznan.transformer.rest;
+package pl.put.poznan.bootstrapbuilder.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-import pl.put.poznan.transformer.logic.BootstrapBuilder;
-
-import java.util.Arrays;
 
 
 @RestController
