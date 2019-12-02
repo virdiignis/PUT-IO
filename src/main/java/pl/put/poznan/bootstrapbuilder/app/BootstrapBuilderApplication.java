24f37d3316dbdd851adc0da0465cf98f0afecf9c
unknown <arekmichalski@icloud.com>
2019-12-02 15:26:40 +0100

    javadoc

--- src/main/java/pl/put/poznan/bootstrapbuilder/app/BootstrapBuilderApplication.java
+++ src/main/java/pl/put/poznan/bootstrapbuilder/app/BootstrapBuilderApplication.java
@@ -5,9 +5,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.bootstrapbuilder.rest"})
-public class BootstrapBuilderApplication {
 
+/**
+* <h1>Bootstrap Builder Application</h1>
+*
+* An application for web programmers 
+* who want to quickly create an application framework 
+* with Bootstrap 4 framework support. 
+* A program that allows you to restore the page code
+* with a header, footer, as well as appropriate META tags 
+* in the context of ordinary SEO, Open Graph and Twitter.
+*
+* @author  IO
+* @version 1.0
+* @since   2019-12-01
+*/
+
+public class BootstrapBuilderApplication {
+	
+	/** 
+    * This is the main method  
+    * which is very important for  
+    * execution for a java program.
+    * @param args Unused.
+    * @return Nothing.
+    */
+	
     public static void main(String[] args) {
+		
         SpringApplication.run(BootstrapBuilderApplication.class, args);
     }
 }
