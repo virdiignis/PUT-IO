fe6493062ac3887c5c4887dee652c38535d514fa
virdiignis <gbryk1@gmail.com>
2020-01-13 17:34:38 +0100

    tests

--- src/main/java/pl/put/poznan/bootstrapbuilder/logic/BootstrapBuilderTest.java
+++ src/main/java/pl/put/poznan/bootstrapbuilder/logic/BootstrapBuilderTest.java
@@ -5,60 +5,51 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BootstrapBuilderTest {
-
-    @Test
-    void withHeader() {
-    }
-
     @Test
     void withFooter() {
     }
 
     @Test
     void withContactDetails() {
+        BootstrapBuilder b = new BootstrapBuilder();
+        b.withContactDetails();
     }
 
-    @Test
-    void withMetaTag() {
-    }
 
     @Test
     void withMetaTagTitle() {
+        BootstrapBuilder b = new BootstrapBuilder();
+        b.withMetaTagTitle("title");
     }
 
-    @Test
-    void withMetaTagType() {
-    }
 
     @Test
     void withMetaTagDescription() {
+        BootstrapBuilder b = new BootstrapBuilder();
+        b.withMetaTagDescription("test");
     }
 
     @Test
     void withMetaTagImage() {
+        BootstrapBuilder b = new BootstrapBuilder();
+        b.withMetaTagImage("test");
     }
 
     @Test
     void withContactCompanyName() {
+        BootstrapBuilder b = new BootstrapBuilder();
+        b.withContactCompanyName("test");
     }
 
     @Test
     void withContactInformation() {
+        BootstrapBuilder b = new BootstrapBuilder();
+        b.withContactInformation("test");
     }
 
     @Test
     void withContactAddress() {
-    }
-
-    @Test
-    void withContactEmail() {
-    }
-
-    @Test
-    void withContactTelephone() {
-    }
-
-    @Test
-    void build() {
+        BootstrapBuilder b = new BootstrapBuilder();
+        b.withContactAddress("test");
     }
 }
