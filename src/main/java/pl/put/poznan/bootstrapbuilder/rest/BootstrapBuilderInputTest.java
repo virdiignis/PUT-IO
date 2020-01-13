d094ac680b7bf95964a2d5e3b2d948b67651aa7d
virdiignis <gbryk1@gmail.com>
2020-01-13 17:09:39 +0100

    test class for input

---
+++ src/main/java/pl/put/poznan/bootstrapbuilder/rest/BootstrapBuilderInputTest.java
@@ -0,0 +1,26 @@
+package pl.put.poznan.bootstrapbuilder.rest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BootstrapBuilderInputTest {
+
+    @org.junit.jupiter.api.Test
+    void getHeaderType() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setHeaderType() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isAddFooter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAddFooter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isAddContactDetails() {
+    }
+}
