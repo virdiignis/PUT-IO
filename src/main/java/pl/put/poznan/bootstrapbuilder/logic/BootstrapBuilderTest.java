a7accc9c6d5e7b1c433b0290976fb3561745d288
virdiignis <gbryk1@gmail.com>
2020-01-13 17:11:27 +0100

    tests for BootstrapBuilder and BootstrapBuilderResponse

---
+++ src/main/java/pl/put/poznan/bootstrapbuilder/logic/BootstrapBuilderTest.java
@@ -0,0 +1,64 @@
+package pl.put.poznan.bootstrapbuilder.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BootstrapBuilderTest {
+
+    @Test
+    void withHeader() {
+    }
+
+    @Test
+    void withFooter() {
+    }
+
+    @Test
+    void withContactDetails() {
+    }
+
+    @Test
+    void withMetaTag() {
+    }
+
+    @Test
+    void withMetaTagTitle() {
+    }
+
+    @Test
+    void withMetaTagType() {
+    }
+
+    @Test
+    void withMetaTagDescription() {
+    }
+
+    @Test
+    void withMetaTagImage() {
+    }
+
+    @Test
+    void withContactCompanyName() {
+    }
+
+    @Test
+    void withContactInformation() {
+    }
+
+    @Test
+    void withContactAddress() {
+    }
+
+    @Test
+    void withContactEmail() {
+    }
+
+    @Test
+    void withContactTelephone() {
+    }
+
+    @Test
+    void build() {
+    }
+}
---
+++ src/main/java/pl/put/poznan/bootstrapbuilder/rest/BootstrapBuilderResponseTest.java
@@ -0,0 +1,20 @@
+package pl.put.poznan.bootstrapbuilder.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BootstrapBuilderResponseTest {
+
+    @Test
+    void getResponse() {
+    }
+
+    @Test
+    void setResponse() {
+    }
+
+    @Test
+    void toJson() {
+    }
+}
